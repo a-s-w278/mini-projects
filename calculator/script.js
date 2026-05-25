691c9509ddfda1a252def11b3f64c131ae5077ee
Aswin Suresh <aswinsuresh.viiid@gmail.com>
2026-05-25 12:45:19 +0530

    calculator

---
+++ calculator/script.js
@@ -0,0 +1,45 @@
+const displayjs=document.querySelector(".display");
+const backspacejs=document.querySelector(".backspace");
+const buttonsjs=document.querySelectorAll(".button-container button");
+function calculate(button)
+{
+    const value=button.textContent;
+    if(value==="=")
+    {
+        try
+        {
+            out=eval(displayjs.value);
+            if(isNaN(out) || !isFinite(out))
+            {
+                displayjs.value="Error";
+            }
+            else
+            {
+                displayjs.value=out;
+            }
+        }
+        catch
+        {
+            displayjs.value="Error";
+        }
+
+    }
+    else if(value==="C")
+    {
+        displayjs.value="";
+    }
+    else
+    {
+        displayjs.value+=value;
+    }
+}
+buttonsjs.forEach(function(button)
+{
+    button.addEventListener("click",function(){calculate(button);});
+});
+function back()
+{
+    let updated=displayjs.value.slice(0,-1);
+    displayjs.value=updated;
+}
+backspacejs.addEventListener("click",back);
