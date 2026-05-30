bd8100f996ff337fc7e458e0ead093967108617c
Aswin Suresh <aswinsuresh.viiid@gmail.com>
2026-05-30 23:48:22 +0530

    improve layout,appearance

--- weather-app/script.js
+++ weather-app/script.js
@@ -3,15 +3,18 @@ const icon=document.querySelector(".icon");
 const temp=document.querySelector(".temp");
 const city=document.querySelector(".city");
 const condition=document.querySelector(".condition");
-const humidity=document.querySelector(".humidity-value");
-const wind=document.querySelector(".wind-speed");
-const feel=document.querySelector(".feels-like-value");
+const humidity=document.querySelector(".humidity");
+const wind=document.querySelector(".wind");
+const feel=document.querySelector(".feels-like");
+const weather_card=document.querySelector(".weather-card");
+const details=document.querySelector(".details");
 
 async function getWeather(event)
 {
     try
     {
-        const cityname=searchbox.value;
+        const cityname=searchbox.value.trim();
+        if(cityname==="") return;
         const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&APPID=${apiKey}&units=metric`);
         if(!response.ok) throw new Error("city not found");
         const data=await response.json();
@@ -20,9 +23,11 @@ async function getWeather(event)
         temp.textContent=`${Math.round(data.main.temp)}°C`;
         city.textContent=data.name;
         condition.textContent=data.weather[0].description;
-        humidity.textContent=`${data.main.humidity}%`;
-        wind.textContent=`${data.wind.speed} km/h`;
-        feel.textContent=`${data.main.feels_like}°C`;
+        humidity.textContent=`💧${data.main.humidity}%`;
+        wind.textContent=`🌬️${data.wind.speed} m/s`;
+        feel.textContent=`🤗${data.main.feels_like}°C`;
+        weather_card.style.display="flex";
+        details.style.display="flex";
     }
     catch(error)
     {
